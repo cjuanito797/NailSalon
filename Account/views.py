916757f583bfbd2a9a734acdd45d6a3a9d09ede1
Juan <jlizarraga@unomaha.edu>
2022-09-02 11:42:16 -0500

    commit

--- Account/views.py
+++ Account/views.py
@@ -1,10 +1,41 @@
 from django.shortcuts import render
+from datetime import date
+import calendar
+
 from .models import Technician
 
+
 # Create your views here.
-def home (request):
-    techs = Technician.objects.all()
-    return render (request, "home.html", {"techs" : techs})
+def home(request):
+    # print all available technicians for that day.
+    curr_date = date.today ( )
+    dayOfWeek = calendar.day_name[curr_date.weekday ( )]
+
+    if dayOfWeek == 'Monday':
+        techs = Technician.objects.filter(schedule__monday_availability=True)
+
+    elif dayOfWeek == 'Tuesday':
+        techs = Technician.objects.filter(schedule__tuesday_availability=True)
+
+    elif dayOfWeek == 'Wednesday':
+        techs = Technician.objects.filter(schedule__wednesday_availability=True)
+
+    elif dayOfWeek == 'Thursday':
+        techs = Technician.objects.filter(schedule__thursday_availability=True)
+
+    elif dayOfWeek == 'Friday':
+        techs = Technician.objects.filter(schedule__friday_availability=True)
+
+    elif dayOfWeek == 'Saturday':
+        techs = Technician.objects.filter(schedule__saturday_availability=True)
+
+    else:
+        techs = Technician.objects.filter(schedule__sunday_availability=True)
+
+
+
+    return render (request, "home.html", {"techs": techs, "dayOfWeek" : dayOfWeek})
+
 
 def mainRegister(request):
-    return render(request, "home.html")
+    return render (request, "home.html")
