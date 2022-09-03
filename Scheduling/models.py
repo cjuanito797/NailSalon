48bd7d215332b851d6f4f513b16e0db26f5215e7
Juan <jlizarraga@unomaha.edu>
2022-09-02 21:52:22 -0500

    commit

--- Scheduling/models.py
+++ Scheduling/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from datetime import date
+import calendar
 
 
 # Create your models here.
@@ -12,78 +14,103 @@ class TechnicianSchedule (models.Model):
     monday_timeOut = models.TimeField ( )
 
     # for monday, link a time slot availability model, for this instance
-    monday_timeSlots = models.ForeignKey("Scheduling.timeSlots",
-                                             related_name="mondayTimeSlots",
-                                             on_delete=models.CASCADE,
-                                             default=False)
-
-
-    tuesday_availability = models.BooleanField(default=False)
-    tuesday_time_In = models.TimeField()
-    tuesday_time_Out = models.TimeField()
-
-    tuesday_timeSlots = models.ForeignKey ("Scheduling.timeSlots",
-                                          related_name="tuesdayTimeSlots",
+    monday_timeSlots = models.ForeignKey ("Scheduling.timeSlots",
+                                          related_name="mondayTimeSlots",
                                           on_delete=models.CASCADE,
                                           default=False)
 
+    tuesday_availability = models.BooleanField (default=False)
+    tuesday_time_In = models.TimeField ( )
+    tuesday_time_Out = models.TimeField ( )
+
+    tuesday_timeSlots = models.ForeignKey ("Scheduling.timeSlots",
+                                           related_name="tuesdayTimeSlots",
+                                           on_delete=models.CASCADE,
+                                           default=False)
 
     wednesday_availability = models.BooleanField (default=False)
     wednesday_time_In = models.TimeField ( )
     wednesday_time_Out = models.TimeField ( )
 
     wednesday_timeSlots = models.ForeignKey ("Scheduling.timeSlots",
-                                           related_name="wednesdayTimeSlots",
-                                           on_delete=models.CASCADE,
-                                           default=False)
+                                             related_name="wednesdayTimeSlots",
+                                             on_delete=models.CASCADE,
+                                             default=False)
 
     thursday_availability = models.BooleanField (default=False)
     thursday_time_In = models.TimeField ( )
     thursday_time_Out = models.TimeField ( )
 
     thursday_timeSlots = models.ForeignKey ("Scheduling.timeSlots",
-                                           related_name="thursdayTimeSlots",
-                                           on_delete=models.CASCADE,
-                                           default=False)
+                                            related_name="thursdayTimeSlots",
+                                            on_delete=models.CASCADE,
+                                            default=False)
 
     friday_availability = models.BooleanField (default=False)
     friday_time_In = models.TimeField ( )
     friday_time_Out = models.TimeField ( )
 
     friday_timeSlots = models.ForeignKey ("Scheduling.timeSlots",
-                                           related_name="fridayTimeSlots",
-                                           on_delete=models.CASCADE,
-                                           default=False)
+                                          related_name="fridayTimeSlots",
+                                          on_delete=models.CASCADE,
+                                          default=False)
 
     saturday_availability = models.BooleanField (default=False)
     saturday_time_In = models.TimeField ( )
     saturday_time_Out = models.TimeField ( )
 
     saturday_timeSlots = models.ForeignKey ("Scheduling.timeSlots",
-                                           related_name="saturdayTimeSlots",
-                                           on_delete=models.CASCADE,
-                                           default=False)
+                                            related_name="saturdayTimeSlots",
+                                            on_delete=models.CASCADE,
+                                            default=False)
 
     sunday_availability = models.BooleanField (default=False)
     sunday_time_In = models.TimeField ( )
     sunday_time_Out = models.TimeField ( )
 
     sunday_timeSlots = models.ForeignKey ("Scheduling.timeSlots",
-                                           related_name="sundayTimeSlots",
-                                           on_delete=models.CASCADE,
-                                           default=False)
+                                          related_name="sundayTimeSlots",
+                                          on_delete=models.CASCADE,
+                                          default=False)
 
     def __str__(self):
         return self.tech + "\'s schedule"
 
+    # get current days time slots, so determine which time slots we want to return for displaying of schedules.
+    def getTodaysTimeSlots(self):
+        # return the correct time slots dependent on the day of the week.
+        curr_date = date.today ( )
+        dayOfWeek = calendar.day_name[curr_date.weekday ( )]
+
+        if dayOfWeek == 'Monday':
+            return self.monday_timeSlots
+
+        elif dayOfWeek == 'Tuesday':
+            return self.tuesday_timeSlots
+
+        elif dayOfWeek == 'Wednesday':
+            return self.wednesday_timeSlots
+
+        elif dayOfWeek == 'Thursday':
+            return self.thursday_timeSlots
+
+        elif dayOfWeek == 'Friday':
+            return self.friday_timeSlots
+
+        elif dayOfWeek == 'Saturday':
+            return self.saturday_timeSlots
+
+        else:
+            return self.sunday_timeSlots
+
 
 class timeSlots (models.Model):
     tech = models.EmailField (_ ('email'), unique=False)
-    dayOfWeek = models.CharField(blank=False, max_length=9)
+    dayOfWeek = models.CharField (blank=False, max_length=9)
     nine_00_am = models.BooleanField (default=False)
-    _9_15am = models.BooleanField (default=False)
-    _9_30am = models.BooleanField(default=False)
-    _9_45am = models.BooleanField(default=False)
+    nine_15am = models.BooleanField (default=False)
+    nine_30am = models.BooleanField (default=False)
+    nine_45am = models.BooleanField (default=False)
 
     _10_00_am = models.BooleanField (default=False)
     _10_15am = models.BooleanField (default=False)
@@ -119,7 +146,6 @@ class timeSlots (models.Model):
     _4_15pm = models.BooleanField (default=False)
     _4_30pm = models.BooleanField (default=False)
     _4_45pm = models.BooleanField (default=False)
-
 
     def __str__(self):
         return self.tech + "\'s" + " " + self.dayOfWeek + " Availability"
