5dfe2832152c81f6f578a2110cf0f964beee661c
ncwroblewski <99614575+ncwroblewski@users.noreply.github.com>
2022-09-09 11:41:31 -0500

    Update urls.py

--- Account/urls.py
+++ Account/urls.py
@@ -7,6 +7,7 @@ app_name = 'account'
 
 urlpatterns = [
     path("", views.home, name="home"),
+    re_path (r'^customerView/$', views.customerView, name='customerView'),
     path("registration/", views.registration_view.as_view(), name="user_registration"),
     path("login/", views.user_login, name="user_login"),
     path("availableTechs/", views.availableTechs, name="availableTechs")
