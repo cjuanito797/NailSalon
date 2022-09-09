904a70c7da704bb8b46f86770fdb9b039aebfb00
Juan <jlizarraga@unomaha.edu>
2022-09-08 21:44:22 -0500

    commit

--- Account/views.py
+++ Account/views.py
@@ -1,20 +1,12 @@
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
 from datetime import date
 import calendar
-
-from .forms import LoginForm
-from .models import Technician
-
 from django.urls import reverse
 from django.views.generic import FormView
 from django.http import HttpResponse
-
-from .forms import RegistrationForm
-from .forms import LoginForm
+from .forms import RegistrationForm, LoginForm
 from .models import Technician
 
 # Create your views here.
@@ -50,27 +42,6 @@ def availableTechs(request):
     return render(request, "home.html", {"techs": techs, "dayOfWeek": dayOfWeek})
 
 
-def user_login(request):
-    form = LoginForm(request.POST)
-    if form.is_valid():
-
-        cd = form.cleaned_data
-        user = authenticate(request,
-                            username=cd['email'],
-                            password=cd['password'])
-        if user is not None:
-            if user.is_active:
-                login(request, user)
-                return render(redirect('account:home'))
-            else:
-                return HttpResponse('Disabled Account')
-        else:
-            return HttpResponse('Invalid Login')
-    else:
-        form = LoginForm()
-    return render(request, 'registration/LoginForm.html', {'form': form})
-
-    return render (request, "availableTechs.html", {"techs": techs, "dayOfWeek" : dayOfWeek})
 
 
 def user_login(request):
@@ -94,7 +65,3 @@ def user_login(request):
     return render(request, 'registration/LoginForm.html', {'form': form})
 
     return render (request, "availableTechs.html", {"techs": techs, "dayOfWeek" : dayOfWeek})
-
-
-def mainRegister(request):
-    return render (request, "availableTechs.html")
