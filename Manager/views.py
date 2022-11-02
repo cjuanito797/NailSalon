4d9d27c7942ed2117aec94d12d38e2eb4240d5d6
KhTran0510 <93550978+KhTran0510@users.noreply.github.com>
2022-11-02 13:27:15 -0500

    refactor home def

--- Manager/views.py
+++ Manager/views.py
@@ -12,75 +12,56 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def home(request):
     if request.method == "POST":
+        id = (int) (request.POST['appointment_id'])
         
-        id = (int) (request.POST['appointment'])
+        sale_control = Control.C_Sale(s_btn=request.POST['sale_btn'], id=id) if 'sale_btn' in request.POST else None
+        appointment_control = Control.C_Appointment(a_btn=request.POST['appointment_btn'], id=id) if 'appointment_btn' in request.POST else None
         
-        sale_control = Control.C_Sale(s_btn=request.POST['sale_btn']) if 'sale_btn' in request.POST else None
-        appointment_control = Control.C_Appointment(a_btn=request.POST['appointment_btn']) if 'appointment_btn' in request.POST else None
+        packet = display(id)
+        return render(request, 'home.html', packet)
+    
+    else:
+        packet = display(None)
+        return render(request, 'home.html', packet)
         
-        # Query Appointment
-        appointment_query = Appointment.objects.all().values_list('customer', 'start_time', 'end_time', 'totalCharge', 'id')
-        appointment_list = []
-        for a in appointment_query:
-            appointment = list(a)
-            appointment[0] = User.objects.filter(id=a[0]).values_list("first_name", "last_name")[0]
-            if appointment[4] == id:
-                appointment.append("checked")
-            else:
-                appointment.append("")   
-            appointment_list.append(appointment)
-        # Query Sale
-        sale_query = Sale.objects.filter(appointment=id).values_list('technician', 'service')
-        # Query Tech    
-        tech_query = Technician.objects.all().values_list('user')
-        tech_list = []
-        for t in tech_query:
-            tech = list(t)
-            tech.append(User.objects.filter(id=a[0]).values_list("first_name", "last_name")[0])
-            tech_list.append(tech)
-            
-        print(tech_list)
-        
-        return render(request, 'home.html', {
+def display(id):
+    # Query Appointment
+    appointment_query = Appointment.objects.all().values_list('customer', 'start_time', 'end_time', 'totalCharge', 'id')
+    appointment_list = []
+    for a in appointment_query:
+        appointment = list(a)
+        appointment[0] = User.objects.filter(id=a[0]).values_list("first_name", "last_name")[0]
+        if appointment[4] == id:
+            appointment.append("checked")
+        else:
+            appointment.append("")   
+        appointment_list.append(appointment)
+    if id is None:
+        appointment_list[0][5] = "checked"
+    # Query Tech    
+    tech_query = Technician.objects.all().values_list('user')
+    tech_list = []
+    for t in tech_query:
+        tech = list(t)
+        tech.append(User.objects.filter(id=a[0]).values_list("first_name", "last_name")[0])
+        tech_list.append(tech)
+    # Query Sale
+    sale_query = Sale.objects.filter(appointment=id).values_list('technician', 'service')
+    return {
             'appointments': appointment_list,
             'sales': sale_query,
             'technicians': tech_list
-        })
+    }
         
-    else:
-        # Query Appointment
-        appointment_list = Appointment.objects.all()
-        appointment_list = Appointment.objects.all().values_list('customer', 'start_time', 'end_time', 'totalCharge', 'id')
-        appointment_arr = []
-        for a in appointment_list:
-            appointment = list(a)
-            appointment[0] = User.objects.filter(id=a[0]).values_list("first_name", "last_name")[0]
-            appointment.append("")
-            appointment_arr.append(appointment)
-        appointment_arr[0][5] = "checked"
-        
-        # Query Tech    
-        tech_query = Technician.objects.all().values_list('user')
-        tech_list = []
-        for t in tech_query:
-            tech = list(t)
-            tech.append(User.objects.filter(id=a[0]).values_list("first_name", "last_name")[0])
-            tech_list.append(tech)
-            
-        return render(request, 'home.html', {
-            'appointments': appointment_arr,
-            'technicians': tech_list,
-        })
-     
-
+    
 class Control:
     def __init__(self) -> None:
         pass
     
     class C_Appointment:
-        def __init__(self, a_btn) -> None:
+        def __init__(self, a_btn, id) -> None:
             self.btn = a_btn
-            print("appointment")
+            print(f"appointment: {id}")
             print(self.btn)
             if a_btn == 'Trigger':
                 pass
@@ -99,9 +80,9 @@ class Control:
             pass
         
     class C_Sale:
-        def __init__(self, s_btn) -> None:
+        def __init__(self, s_btn, id) -> None:
             self.btn = s_btn
-            print("sale")
+            print(f"sale: {id}")
             print(self.btn)
             if s_btn == 'Cancel':
                 pass
