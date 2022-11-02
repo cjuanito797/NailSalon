2c84cd5f5a692a5595785b305ee8112dfa8dc4ef
KhTran0510 <93550978+KhTran0510@users.noreply.github.com>
2022-11-01 22:16:23 -0500

    display appointments as radio button

--- Manager/views.py
+++ Manager/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
 from Appointments.models import Appointment
+from Account.models import User
 
 # Create your views here.
 def home(request):
-    # appointment_list = Appointment.objects.all().values_list("customer", "start_time", "end_time", "totalCharge")
+    
     appointment_list = Appointment.objects.all()
+    
+    appointment_list = Appointment.objects.all().values_list('customer', 'start_time', 'end_time', 'totalCharge', 'id')
+
+    appointment_arr = []
+    for a in appointment_list:
+        appointment = list(a)
+        appointment[0] = User.objects.filter(id=a[0]).values_list("first_name", "last_name")[0]
+        appointment_arr.append(appointment)
+        
     return render(request, 'home.html', {
-        'appointment': appointment_list,
+        'appointments': appointment_arr,
     })
