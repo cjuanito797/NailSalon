cc899b1f07cc0d12046e5c9c65590f6f347d9c6e
unknown <dha@unomaha.edu>
2022-11-01 21:12:05 -0500

    for Khoa

--- Manager/views.py
+++ Manager/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from Appointments.models import Appointment
+
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    # appointment_list = Appointment.objects.all().values_list("customer", "start_time", "end_time", "totalCharge")
+    appointment_list = Appointment.objects.all()
+    return render(request, 'home.html', {
+        'appointment': appointment_list,
+    })
