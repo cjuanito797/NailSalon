facab1656044ce175e4e4305e332bff85db8c81a
KhTran0510 <93550978+KhTran0510@users.noreply.github.com>
2022-11-05 01:42:16 -0500

    remove unuse files

--- Manager/urls.py
+++ Manager/urls.py
@@ -1,5 +1,5 @@
-from django.contrib.auth import views as auth_views
-from django.urls import path, re_path
+from django.contrib.auth import views
+from django.urls import path
 
 from . import views
 
