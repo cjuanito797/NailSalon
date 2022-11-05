2d53076b59a6ccde97e576210a14a346d9e769c2
unknown <dha@unomaha.edu>
2022-11-05 15:10:37 -0500

    random tech queue

--- helper/techs_queue.py
+++ helper/techs_queue.py
@@ -11,7 +11,6 @@ from Scheduling.models import timeSlots
 from timeslot_process import _Process
 from Appointments.models import Sale
 
-COMMAND = {'wait': ':WAIT\n', 'work': ':WORK\n'}
 _WAIT_QUEUE = []
 _WORK_QUEUE = []
 
@@ -37,62 +36,104 @@ def build_fresh_wait_queue(today: date, onday_techs: list): # change to queue of
 def build_wait_queue():
     a = Sale.objects.filter(status='working').values_list('technician', 'status')[0]
     print(a)
-    
 
 
-def main():
-    f = open("helper/temp", "r")
-    lines = f.readlines()
+
+
+# Split formatted string data into tuple  "email:priority" -> ("email", priority)
+def to_tuple(line:str) -> tuple:
+    temp_str = []
+    temp_num =  []
+    tup = []
     z = 0
+    for c in line:
+        if (c != ':' and z == 0):
+            temp_str.append(c)
+        elif (c == "\n"):
+            break
+        else:
+            z = 1
+            if (z==1):
+                temp_num = c
+    
+    return ( ((''.join(temp_str)), int(temp_num)) )
+# Function to sort the list of tuples by its second item
+def Sort_Tuple(lst):
+    global _WAIT_QUEUE
+    _WAIT_QUEUE = (sorted(lst, key = lambda x: x[1]))
+
+
+
+def read_temp():
+    f = open("helper/temp1", "r")
+    lines = f.readlines()
+    f.close()
+    read_flag = 0
+    wait_tuple_list = []
     for line in lines:
         line = line.replace('\n', '')
         if (line == "_WAIT"):
             pass
         else:
-            if line != "_WORK" and z == 0:
-                temp = line[1]
-                _WAIT_QUEUE.append(temp)
+            if line != "_WORK" and read_flag == 0:
+                _WAIT_QUEUE.append(to_tuple(line))
             elif line == "_WORK":
-                z = 1
+                read_flag = 1
             else:
-                _WORK_QUEUE.append(line)
+                _WORK_QUEUE.append(to_tuple(line))
+
+def write_temp():
+    data = ""
+    data += "_WAIT\n"
+    for i in _WAIT_QUEUE:
+        data += f"{str(i[0])}:{str(i[1])}\n"
+    data += "_WORK\n"
+    for i in _WORK_QUEUE:
+        data += f"{str(i[0])}:{str(i[1])}\n"
+    f = open("helper/temp1", "w")
+    f.write(data)
+    f.close()
+
+def save_queue(queue):
+    Sort_Tuple(queue)
+    write_temp()
+
+def wait_to_work():
+    if len(_WAIT_QUEUE) >=1:
+        a = _WAIT_QUEUE.pop(0)
+        _WORK_QUEUE.append((a[0], a[1]+1))
+        save_queue(_WAIT_QUEUE)
+        return len(_WAIT_QUEUE)
+    else:
+        return 0
+
+def work_to_wait(email):
+    if len(_WORK_QUEUE) >=1:
+        for i in _WORK_QUEUE:
+            if i[0] == email:
+                a = _WORK_QUEUE.remove(i)
+                _WAIT_QUEUE.append(i)
+        save_queue(_WAIT_QUEUE)
+        return len(_WAIT_QUEUE)
+    else:
+        return 0
+
+
+
+
+
+
+def main():
+    global _WAIT_QUEUE
+    read_temp()
     
-    a = _WAIT_QUEUE.pop(0)
-    _WORK_QUEUE.append(a)
-    
-    print(_WAIT_QUEUE)
-    print(_WORK_QUEUE)
-    
-'''
-# Function to sort the list of tuples by its second item
-def Sort_Tuple(tup):
-     
-    # getting length of list of tuples
-    lst = len(tup)
-    for i in range(0, lst):
-         
-        for j in range(0, lst-i-1):
-            if (tup[j][1] > tup[j + 1][1]):
-                temp = tup[j]
-                tup[j]= tup[j + 1]
-                tup[j + 1]= temp
-    return tup
- 
-# Driver Code
-tup =[('for', 24), ('is', 10), ('Geeks', 28),
-      ('Geeksforgeeks', 5), ('portal', 20), ('a', 15)]
-       
-print(Sort_Tuple(tup))
-'''
-            
-  
-
-
-
+    wait_to_work()
+    work_to_wait('c@email.com')
 
 
 
 '''
+Khoa's stuff:
     process = _Process(date(2022, 11, 12))
     
     onday_techs = []
