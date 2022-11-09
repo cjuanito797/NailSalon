e4e6e8dde65b3693813267cfcf0d2da32b44cc90
KhTran0510 <93550978+KhTran0510@users.noreply.github.com>
2022-11-09 17:47:46 -0600

    Add New Technician feature

--- Manager/forms.py
+++ Manager/forms.py
@@ -1,4 +1,66 @@
 from django import forms
+from django.core.validators import MinLengthValidator
 
-class AppointmentID(forms.Form):
-    id = forms.CharField(label='id')
+class NewTechnicianForm(forms.Form):
+    STATE_OPTIONS = (
+        ('Alabama', 'Alabama'),
+        ('Alaska', 'Alaska'),
+        ('Arizona', 'Arizona'),
+        ('Arkansas', 'Arkansas'),
+        ('California', 'California'),
+        ('Colorado', 'Colorado'),
+        ('Conneticut', 'Conneticut'),
+        ('Deleware', 'Deleware'),
+        ('Florida', 'Florida'),
+        ('Georgia', 'Georgia'),
+        ('Hawaii', 'Hawaii'),
+        ('Idaho', 'Idaho'),
+        ('Illinois', 'Illinois'),
+        ('Indiana', 'Indiana'),
+        ('Iowa', 'Iowa'),
+        ('Kansas', 'Kansas'),
+        ('Kentucky', 'Kentucky'),
+        ('Louisiana', 'Louisiana'),
+        ('Maine', 'Maine'),
+        ('Maryland', 'Maryland'),
+        ('Massachusetts', 'Massachusetts'),
+        ('Michigan', 'Michigan'),
+        ('Minnesota', 'Minnesota'),
+        ('Mississippi', 'Mississippi'),
+        ('Missouri', 'Missouri'),
+        ('Montana', 'Montana'),
+        ('Nebraska', 'Nebraska'),
+        ('Nevada', 'Nevada'),
+        ('New Hampshire', 'New Hampshire'),
+        ('New Jersey', 'New Jersey'),
+        ('New Mexico', 'New Mexico'),
+        ('New York', 'New York'),
+        ('North Carolina', 'North Carolina'),
+        ('North Dakota', 'North Dakota'),
+        ('Ohio', 'Ohio'),
+        ('Oklahoma', 'Oklahoma'),
+        ('Oregon', 'Oregon'),
+        ('Pennsylvania', 'Pennsylvania'),
+        ('Rhode Island', 'Rhode Island'),
+        ('South Carolina', 'South Carolina'),
+        ('South Dakota', 'South Dakota'),
+        ('Tennessee', 'Tennessee'),
+        ('Texas', 'Texas'),
+        ('Utah', 'Utah'),
+        ('Vermont', 'Vermont'),
+        ('Virginia', 'Virgina'),
+        ('Washington', 'Washington'),
+        ('West Virginia', 'West Virginia'),
+        ('Wisconsin', 'Wisconsin'),
+        ('Wyoming', 'Wyoming'),
+    )
+    
+    email = forms.EmailField (max_length=254)
+    first_name = forms.CharField (max_length=200)
+    last_name = forms.CharField (max_length=200)
+    street_num = forms.CharField (max_length=20)
+    
+    state = forms.CharField (max_length=15)
+    zipcode = forms.CharField (max_length=5, validators=[MinLengthValidator (5)])
+    city = forms.CharField (max_length=20)
+    phoneNumber = forms.CharField (max_length=10, validators=[MinLengthValidator (10)])
