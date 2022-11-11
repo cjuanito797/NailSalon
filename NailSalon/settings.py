546bda977325899ab1b83438f06b895442b83627
unknown <dha@unomaha.edu>
2022-11-11 17:43:05 -0600

    pull Dev

--- NailSalon/settings.py
+++ NailSalon/settings.py
@@ -139,7 +139,3 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'cjuangas17@gmail.com'
 EMAIL_HOST_PASSWORD = 'frdkmcgbeiwpjrzh'
-
-
-MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
